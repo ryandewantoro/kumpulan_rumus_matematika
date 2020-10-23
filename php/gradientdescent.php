ea680c5b42f8d37f2872fde34c65f305bcce701a
Ryan Dewantoro <riiean@umsida.ac.id>
2020-10-23 08:51:58 +0700

    Update gradientdescent.php

--- php/gradientdescent.php
+++ php/gradientdescent.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head><meta charset="utf-8" />
-<title>Pandit_Meeta_PS4</title>
+<title>gradient descent</title>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.1.10/require.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
